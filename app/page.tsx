514416595d63e85a53e5ae8a0219efa82a42319f
Eric Harth <ericharth@gmail.com>
2025-10-01 15:17:17 +1000

    Implement structural updates and optimizations across multiple modules

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import Guest from "@/components/Guest";
+import { currentUser } from "@clerk/nextjs/server";
+import React from "react";
 
-export default function HomePage() {
-  return (
-    <>
-      <h1 className="text-4xl">Sleep tracker</h1>
-      <div className="text-red-500">HomePage</div>
-    </>
-  )
+export default async function HomePage() {
+    const user = await currentUser();
+    console.log("user", user);
+
+    if (!user) {
+        return <Guest />;
+    }
+
+    return (
+        <>
+            <h1 className="text-4xl">Sleep tracker</h1>
+            <div className="text-red-500">HomePage</div>
+        </>
+    );
 }
